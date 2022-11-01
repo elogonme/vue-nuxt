43a80bc4ab30f4d29f24c71e34f821eb7a59bfb7
Eldar Humbatov <elogon@gmail.com>
2022-11-01 13:41:45 -0400

    exercise 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+  modules: [
+    [
+      "@storyblok/nuxt",
+      {
+        accessToken: "aCLQYrzlSySeAAqRiqsv4Att",
+        // If Server Location === US then:
+        apiOptions: { region: "us" },
+      },
+    ],
+    "@nuxtjs/tailwindcss",
+  ],
+});
---
+++ pages/[...slug].vue
@@ -0,0 +1,20 @@
+<script setup>
+const { slug } = useRoute().params;
+
+const resolveRelations = ["popular-articles.articles"];
+
+const story = await useAsyncStoryblok(
+  slug && slug.length > 0 ? slug.join("/") : "home",
+  {
+    version: "draft",
+    resolve_relations: resolveRelations, // Resolved for the API request
+  },
+  {
+    resolveRelations, // Resolved for the Storyblok Bridge
+  }
+);
+</script>
+ 
+<template>
+  <StoryblokComponent v-if="story" :blok="story.content" />
+</template>
